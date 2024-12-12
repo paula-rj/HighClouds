d3ba50189a06729e226280ae140a10cf918d0e02
paularomero <eepvrj@leeds.ac.uk>
2024-12-12 17:53:14 +0000

    CI bar plots for itcz and ep, created class in methods

--- methods.py
+++ methods.py
@@ -1,7 +1,10 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Author: Paula Romero Jure
 # Imports
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
 import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 from scipy import stats
 
@@ -11,6 +14,7 @@ import xarray as xa
 # Var names convention
 # varDescription_place_timeRange
 # Time range eg: clim, monthlyclim, annual, interannual, enso
+
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Functions
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -23,8 +27,9 @@ def anomaly(ds):
 
 def netCRE(lwds, swds, times=False, cc=None):
     """Calculates a net CRE kernel.
-    If times, calculates Net CRE (or R)
-    Arguments:
+    If times, calculates Net CRE (or cloud-induced radiative anomaly R)
+
+    Parameters:
     ----------
     lwds: xr.Dataset
         Dataset containing long-wave TOA rad values. Must be trimmed
@@ -39,52 +44,12 @@ def netCRE(lwds, swds, times=False, cc=None):
 
     if times:
         cc_anom = anomaly(cc)
-        R = K_trop * cc_anom
+        return K_trop * cc_anom
     else:
         return K_trop
 
 
-def feedback_amount(area, K, sst_anom):
-    cTot = area.sum(["press", "opt"])
-    cc_anom = area.groupby("time.month") - area.groupby("time.month").mean("time")
-    cTot_anom = cc_anom.sum(["press", "opt"])
-
-    K_0_hc = (((area / cTot)) * K).sum(["opt", "press"])
-    Ramt_anom = K_0_hc * cTot_anom
-    return stats.linregress(sst_anom, Ramt_anom.mean("lat")).slope
-
-
-def hc_feedbacks(area, K, sst_anom, which):
-
-    i = ["press", "opt"]
-    if which not in i:
-        raise NameError(f"Decompsitions only in {i} dims")
-
-    opp = i[i.index(which) - 1]
-
-    cTot = area.sel(press=[4, 5, 6]).sum(["press", "opt"])
-    cc_anom = area.sel(press=[4, 5, 6]).groupby("time.month") - area.sel(
-        press=[4, 5, 6]
-    ).groupby("time.month").mean("time")
-    cTot_anom = cc_anom.sel(press=[4, 5, 6]).sum(["press", "opt"])
-
-    cc_ast = cc_anom - (area.sel(press=[4, 5, 6]) / cTot) * cTot_anom
-
-    ci_Tot = (area.sel(press=[4, 5, 6]) / cTot).sum(which)
-    Ktau = K.sel(press=[4, 5, 6]) * ci_Tot.sum(opp)
-
-    K_prima = (Ktau * ci_Tot).sum(opp)
-
-    casi_R = K_prima * cc_ast.sum(opp)
-
-    R_altitude = casi_R.sum(which)
-
-    feedback_i = stats.linregress(sst_anom, R_altitude.mean("lat")).slope
-
-    return feedback_i
-
-
-def correlations(sst, var):
+def correlations(sst, var, plot=True):
     """Calcula correlaciones para time series y plotea las dos juntas.
     lat, lon y times must be selected
     INputs
@@ -94,44 +59,123 @@ def correlations(sst, var):
     var:xa.DataSet
     EL dataset de la variable con todas las dimensiones"""
     # calcular anomalies para los dos
-    odweight_itcz = (
-        odtropics.sel(opt=[4, 5], lat=slice(0, 20))
-        .weighted(area_tropics.sel(opt=[4, 5], lat=slice(0, 20)))
-        .mean(["opt", "press", "lat", "lon"])
-    )
+    odweight_itcz = var.mean(["opt", "press", "lat", "lon"])
+
     odweight_itcz_anom = odweight_itcz.groupby("time.month") - odweight_itcz.groupby(
         "time.month"
     ).mean("time")
 
     # plot
-    fig, ax1 = plt.subplots(figsize=(10, 6))
+    if plot:
+        fig, ax1 = plt.subplots(figsize=(10, 6))
 
-    color = "tab:red"
-    (p1,) = odweight_itcz_anom.plot(
-        linewidth=0.6,
-        ax=ax1,
-        color=color,
-    )
-    ax1.tick_params(axis="y", labelcolor=color, **tkw)
+        color = "tab:red"
+        (p1,) = odweight_itcz_anom.plot(
+            linewidth=0.6,
+            ax=ax1,
+            color=color,
+        )
+        ax1.tick_params(
+            axis="y",
+            labelcolor=color,
+        )
 
-    ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
-    color = "tab:blue"
-    (p2,) = (
-        sst_anom_latlon.sel(lat=slice(20.5, -0.5), lon=ep)
-        .mean(["lat", "lon"])
-        .plot(linewidth=0.6, ax=ax2, color=color)
-    )  # ax2.plot(t, data2, color=color)
-    ax2.tick_params(axis="y", labelcolor=color, **tkw)
+        ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+        color = "tab:blue"
+        (p2,) = sst.mean(["lat", "lon"]).plot(
+            linewidth=0.6, ax=ax2, color=color
+        )  # ax2.plot(t, data2, color=color)
+        ax2.tick_params(
+            axis="y",
+            labelcolor=color,
+        )
 
-    plt.suptitle(
-        "Thick high clouds OD and SST monthly anomalies - Lat=[0,20], Lon=[120,280]"
-    )
-    ax1.set_ylabel("OD")
-    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        plt.suptitle(
+            "Thick high clouds OD and SST monthly anomalies - Lat=[0,20], Lon=[120,280]"
+        )
+        ax1.set_ylabel("OD")
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
-    ax1.xaxis.set_major_locator(mdates.YearLocator(base=1))
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
-    ax1.grid(visible=True, axis="x")
+        ax1.xaxis.set_major_locator(mdates.YearLocator(base=1))
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+        ax1.grid(visible=True, axis="x")
 
-    plt.show()
-    return corr
+        plt.show()
+
+    return xa.corr(sst, odweight_itcz_anom)
+
+
+class Feedbacks:
+
+    def __init__(self):
+        return None
+
+    def feedback_amount(area, K, sst_anom):
+        cTot = area.sum(["press", "opt"])
+        cc_anom = area.groupby("time.month") - area.groupby("time.month").mean("time")
+        cTot_anom = cc_anom.sum(["press", "opt"])
+
+        K_0_hc = (((area / cTot)) * K).sum(["opt", "press"])
+        Ramt_anom = K_0_hc * cTot_anom
+        return stats.linregress(sst_anom, Ramt_anom.mean("lat")).slope
+
+    def hc_feedbacks(area, K, sst_anom, which):
+
+        i = ["all", "amount", "press", "opt"]
+        if which not in i:
+            raise NameError(f"Decompsitions only in {i} dims")
+
+        cTot = area.sel(press=[4, 5, 6]).sum(["press", "opt"])
+        cc_anom = area.sel(press=[4, 5, 6]).groupby("time.month") - area.sel(
+            press=[4, 5, 6]
+        ).groupby("time.month").mean("time")
+        cTot_anom = cc_anom.sel(press=[4, 5, 6]).sum(["press", "opt"])
+
+        cc_ast = cc_anom - (area.sel(press=[4, 5, 6]) / cTot) * cTot_anom
+
+        opp = i[i.index(which) - 1]
+
+        ci_Tot = (area.sel(press=[4, 5, 6]) / cTot).sum(which)
+        Ktau = K.sel(press=[4, 5, 6]) * ci_Tot.sum(opp)
+
+        K_prima = (Ktau * ci_Tot).sum(opp)
+
+        casi_R = K_prima * cc_ast.sum(opp)
+
+        R_altitude = casi_R.sum(which)
+
+        feedback_i = stats.linregress(sst_anom, R_altitude.mean("lat")).slope
+
+        return feedback_i
+
+    def res_feedbacks(area, K, sst_anom, which):
+
+        i = ["all", "amount", "press", "opt"]
+        if which not in i:
+            raise NameError(f"Decompsitions only in {i} dims")
+
+        cTot = area.sel(press=[4, 5, 6]).sum(["press", "opt"])
+        cc_anom = area.sel(press=[4, 5, 6]).groupby("time.month") - area.sel(
+            press=[4, 5, 6]
+        ).groupby("time.month").mean("time")
+        cTot_anom = cc_anom.sel(press=[4, 5, 6]).sum(["press", "opt"])
+
+        cc_ast = cc_anom - (area.sel(press=[4, 5, 6]) / cTot) * cTot_anom
+
+        opp = i[i.index(which) - 1]
+
+        ci_Tot = (area.sel(press=[4, 5, 6]) / cTot).sum(which)
+        Ktau = K.sel(press=[4, 5, 6]) * ci_Tot.sum(opp)
+        K_prima = (Ktau * ci_Tot).sum(opp)
+
+        ci_Tot = (area.sel(press=[4, 5, 6]) / cTot).sum(which)
+        Ktau = K.sel(press=[4, 5, 6]) * ci_Tot.sum(opp)
+        K_prima = (Ktau * ci_Tot).sum(opp)
+
+        K_R = K - K_prima - K_prima
+
+        R_res = (K_R * cc_ast).sum(["press", "opt"])
+
+        feed_res = stats.linregress(sst_anom, R_res.mean("lat")).slope
+
+        return feed_res
